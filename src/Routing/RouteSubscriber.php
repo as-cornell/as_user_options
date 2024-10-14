f1f2f1abe6181a43cc439ebd490aeecc70c51a81
markewilson <mark.e.wilson@gmail.com>
2024-10-14 09:22:47 -0400

    adds route subscriber and services

---
+++ src/Routing/RouteSubscriber.php
@@ -0,0 +1,24 @@
+namespace Drupal\as_user_options\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+   if ($route = $collection->get('entity.user.edit_form')) {
+      $route->setRequirement('_permission', 'administer users');
+    }
+
+    if ($route = $collection->get('entity.user.canonical')) {
+      $route->setRequirement('_permission', 'administer users');
+    }
+  }
+
+}
